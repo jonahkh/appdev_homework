d6a9f8fd2411591509aba67894fbf5689ea4d5ec
Jonah Howard <johoward@localhost.localdomain>
2019-03-01 16:25:36 -0500

    updated Jenkins File for syntax errors

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
 ./setup_projects.sh $GUID johoward-redhat.com false
 ./setup_dev.sh $GUID
 ./setup_prod.sh $GUID
-./setup_jenkins.sh $GUID https://github.com/jonahkh/advdev_homework.git na311.openshift.opentlc.com
+./setup_jenkins.sh $GUID https://github.com/jonahkh/appdev_homework.git na311.openshift.opentlc.com
