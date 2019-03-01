0e6ce8a66bde028028a4428ed45e9f1a5ab2427d
Jonah Howard <johoward@localhost.localdomain>
2019-03-01 11:39:41 -0500

    another update

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -14,7 +14,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 
 # Set up Jenkins with sufficient resources
 # TBD
-oc new-app --template=jenkins-persistent --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=10Gi --param ENABLE_OAUTH=true  -n ${GUID}-jenkins 
+oc new-app jenkins-persistent --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=10Gi --param ENABLE_OAUTH=true --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins 
 oc set resources dc/jenkins --limits=memory=4Gi,cpu=4 --requests=memory=2Gi,cpu=2 -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
