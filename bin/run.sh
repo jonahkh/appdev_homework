ed735b5b6c79b0524cbaf281043b03f856f6a7b7
Jonah Howard <johoward@localhost.localdomain>
2019-03-04 12:48:44 -0500

    updated resources

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,6 @@
-./setup_projects.sh $GUID johoward-redhat.com false
+GUID=9b89
+
+./setup_projects.sh $GUID johoward-redhat.com true
+./setup_jenkins.sh $GUID https://github.com/jonahkh/appdev_homework.git na311.openshift.opentlc.com
 ./setup_dev.sh $GUID
 ./setup_prod.sh $GUID
-./setup_jenkins.sh $GUID https://github.com/jonahkh/appdev_homework.git na311.openshift.opentlc.com
